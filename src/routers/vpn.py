2e382d1ca843d7db7128794a61f47b6eed5f8361
sakhaline <alexandra.ivva@gmail.com>
2024-02-01 06:29:51 +0200

    Implement vpn routers

---
+++ src/routers/vpn.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, render_template_string, render_template
+
+from ..services.vpn_service import VPNService
+from ..services.site_service import SiteService
+from ..repositories.site_repo import SiteRepository
+from ..extensions import db
+
+
+vpn = Blueprint("vpn", __name__, url_prefix='/vpn')
+
+
+@vpn.route('/<site_name>/<path:original_url>')
+def proxy_site(site_name, original_url) -> str:
+    vpn_service = VPNService(site_name, original_url)
+    page = vpn_service.process_page()
+    received_data_mb = vpn_service.calculate_site_stat()
+    SiteService(SiteRepository(db)).update_site_stat(site_name,
+                                                     received_data_mb)
+    return render_template_string(str(page))
+
+
+@vpn.route("/stat")
+def get_stat():
+    sites = SiteService(SiteRepository(db)).get_all_sites()
+    return render_template("vpn/stat.html", sites=sites)
