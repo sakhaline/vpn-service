57b2cec4b77f971b6c624351b3db45cb78804334
sakhaline <alexandra.ivva@gmail.com>
2024-02-01 06:01:44 +0200

    Update models

--- src/models/sites.py
+++ src/models/sites.py
@@ -2,7 +2,7 @@ from typing import TYPE_CHECKING
 
 from datetime import datetime
 
-from sqlalchemy import ForeignKey, func, text
+from sqlalchemy import ForeignKey, func, text, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.extensions import db
@@ -16,13 +16,16 @@ class Site(db.Model):
     __tablename__ = "sites"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    original_url: Mapped[str] = mapped_column(nullable=False)
-    custom_url: Mapped[str] = mapped_column(nullable=True)
+    url: Mapped[str] = mapped_column(nullable=False)
     name: Mapped[str] = mapped_column(nullable=False)
     page_visits: Mapped[int] = mapped_column(insert_default=text("0"))
     data_sent: Mapped[int] = mapped_column(insert_default=text("0"))
-    data_recieved: Mapped[int] = mapped_column(insert_default=text("0"))
+    data_received: Mapped[float] = mapped_column(insert_default=text("0"))
     created_at: Mapped[datetime] = mapped_column(insert_default=func.now())
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
 
     user: Mapped["User"] = relationship(back_populates="sites")
+
+    __table_args__ = (
+        UniqueConstraint('user_id', 'url', name='unique_user_url'),
+    )
