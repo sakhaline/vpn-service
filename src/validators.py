f78f1b7a151d6f8256cd5b3332e5609c555c11ec
sakhaline <alexandra.ivva@gmail.com>
2024-02-03 17:05:09 +0200

    Update url validator

--- src/validators.py
+++ src/validators.py
@@ -4,6 +4,5 @@ from flask import abort
 def invalid_url_handler():
     error_message = (
         "Invalid URL: No scheme supplied. "
-        "Perhaps you meant https://sdsdsd?"
     )
     abort(400, description=error_message)
